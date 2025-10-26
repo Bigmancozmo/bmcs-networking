1c0ee547e4c050c6569695bbe66f531e4fb83e4b
Bigmancozmo <bigmancozmo@proton.me>
2025-10-26 14:17:48 -0400

    I found the bug

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { ServerRemoteFunction as _ServerRemoteFunction } from "./ServerRemoteFun
 import { ClientRemoteFunction as _ClientRemoteFunction } from "./ClientRemoteFunction";
 import { RemoteEventDefinition as _RemoteEventDefinition } from "./RemoteEvent";
 
-script.FindFirstChild("RemoteFolder")?.Destroy()
-
 export const ServerRemoteFunction = _ServerRemoteFunction;
 export const ClientRemoteFunction = _ClientRemoteFunction;
 export const RemoteEventDefinition = _RemoteEventDefinition;
